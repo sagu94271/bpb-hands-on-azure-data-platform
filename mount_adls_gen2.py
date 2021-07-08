6a8572bd5f77e7328e99b931fa3e003ac600a2e3
Sagar Lad <sagu94271@gmail.com>
2021-07-08 19:53:41 +0200

    Create mount_adls_gen2.py

---
+++ mount_adls_gen2.py
@@ -0,0 +1,11 @@
+configs = {"fs.azure.account.auth.type": "OAuth",
+       "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+       "fs.azure.account.oauth2.client.id": "<appId>",
+       "fs.azure.account.oauth2.client.secret": "<clientSecret>",
+       "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/<tenant>/oauth2/token",
+       "fs.azure.createRemoteFileSystemDuringInitialization": "true"}
+
+dbutils.fs.mount(
+source = "abfss://<container-name>@<storage-account-name>.dfs.core.windows.net/input",
+mount_point = "/mnt/transactiondata",
+extra_configs = configs)
